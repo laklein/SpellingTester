ae2b4f8f6135e4d055dec64c8be05a008120ad5e
kwilcock <karltherunner@gmail.com>
2019-05-01 19:07:12 -0600

    Adds example test and initial database setup

--- App.js
+++ App.js
@@ -2,11 +2,92 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './src/navigation/AppNavigator';
+import { SQLite } from 'expo';
+import _ from 'lodash'
+
+const db = SQLite.openDatabase('spellingTester.db');
 
 export default class App extends React.Component {
   state = {
+    databaseInitializationComplete: false,
     isLoadingComplete: false,
+    tests: [],
   };
+
+  componentDidMount() {
+    this.initializeDatabase()
+    this.loadTests()
+  }
+
+  initializeDatabase() {
+    db.transaction(tx => {
+      tx.executeSql(
+        'create table if not exists tests (id integer primary key not null, name text, completed int, score int);'
+      )
+      tx.executeSql(
+        'create table if not exists questions (id integer primary key not null, test_id int, body text, answer text, foreign key(test_id) references tests(id));'
+      )
+    });
+  }
+
+  loadTests() {
+    db.transaction(tx => {
+      tx.executeSql('select * from tests', [], (tx, { rows: { _array } }) => {
+        if (_.isEmpty(_array)) {
+          this.createExampleTest()
+        }
+        let tests = []
+        _.map(_array, (row) => {
+          tx.executeSql('select * from questions where test_id = ?', [row.id], (tx, { rows: { _array } }) => {
+            tests.push({
+              id: row.id,
+              name: row.name,
+              completed: row.completed,
+              score: row.score,
+              questions: _array,
+            })
+            this.setState({ tests: tests })
+          })
+        })
+      }, (tx, error) =>
+        console.log('Error: ', error)
+      )
+    })
+  }
+
+  createExampleTest() {
+    this.exampleInserts()
+    this.loadTests()
+  }
+
+  exampleInserts() {
+    db.transaction(tx => {
+      tx.executeSql(`insert into tests (name, completed) values ("Example Test", 0);`, [], (tx, result) => {
+        tx.executeSql(`insert into questions (test_id, body, answer) values (${id}, "porcupine", "porcupine");`)
+        tx.executeSql(`insert into questions (test_id, body, answer) values (${id}, "watermelon", "watermelon");`)
+        tx.executeSql(`insert into questions (test_id, body, answer) values (${id}, "television", "television");`)
+      })
+    })
+  }
+
+  exampleUpdate() {
+    db.transaction(tx => {
+      tx.executeSql(`update tests set completed = 1 where id = 1;`);
+    })
+  }
+
+  exampleDelete() {
+    db.transaction(tx => {
+      tx.executeSql(`delete from tests where id = 1;`)
+    })
+  }
+
+  exampleDropTables() {
+    db.transaction(tx => {
+      tx.executeSql(`drop table if exists questions;`)
+      tx.executeSql(`drop table if exists tests;`)
+    })
+  }
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
@@ -21,7 +102,7 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+          <AppNavigator screenProps={{ tests: this.state.tests }}/>
         </View>
       );
     }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {
+  FlatList,
+  Keyboard,
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
+import _ from 'lodash';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -19,26 +24,145 @@ export default class HomeScreen extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      answerInput: '',
+      answerResult: '',
+      currentQuestion: null,
+      currentTest: null,
+      finished: false,
+      score: 0,
+      showTests: false,
+      submitButtonText: 'Check',
+    }
     this.onTakeTest = this.onTakeTest.bind(this)
+    this.onSubmitAnswer = this.onSubmitAnswer.bind(this)
+    this.onSelectTest = this.onSelectTest.bind(this)
+    this.onRetry = this.onRetry.bind(this)
+  }
+
+  onSelectTest(test) {
+    this.setState({ currentTest: test, currentQuestion: test.questions[0] })
   }
 
   onTakeTest() {
-    
+    this.setState({ showTests: true })
+  }
+
+  onSubmitAnswer() {
+    const { answerInput, answerResult, currentQuestion, currentTest, submitButtonText } = this.state
+    if (submitButtonText === 'Next') {
+      const currentIndex = _.findIndex(currentTest.questions, { 'id': currentQuestion.id })
+      const nextQuestion = _.nth(currentTest.questions, currentIndex + 1)
+      if (_.isNil(nextQuestion)) {
+        this.setState({ answerResult: null, currentQuestion: null, finished: true })
+      } else {
+        this.setState({ answerResult: null, currentQuestion: nextQuestion })
+      }
+      this.setState({ submitButtonText: 'Check' })
+    } else {
+      if (_.upperCase(answerInput) === _.upperCase(currentQuestion.answer)) {
+        this.setState({ answerResult: 'You are correct!', score: this.state.score + 1 })
+      } else {
+        this.setState({ answerResult: 'Not quite!' })
+      }
+      this.setState({ submitButtonText: 'Next' })
+    }
+    this.setState({ answerInput: '' })
+  }
+
+  onRetry() {
+    this.setState({
+      answerInput: '',
+      answerResult: '',
+      score: 0,
+      currentQuestion: this.state.currentTest.questions[0],
+      finished: false,
+    })
+  }
+
+  renderMenu() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.testButton} onPress={this.onTakeTest}>
+          <Text style={styles.buttonText}>Take Test</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.createButton} onPress={() => navigate('Links')}>
+          <Text style={styles.buttonText}>Create Test</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  renderTests() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.props.screenProps.tests}
+          keyExtractor={item => _.toString(item.id)}
+          renderItem={({item}) =>
+            <TouchableOpacity style={styles.listItem} onPress={() => this.onSelectTest(item) }>
+              <Text style={styles.listText}>{item.name}</Text>
+            </TouchableOpacity>
+          }
+        />
+      </View>
+    )
+  }
+
+  renderQuestion() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.question}>
+          <Text style={styles.questionText}>Spell {this.state.currentQuestion.body}</Text>
+          <TextInput
+            autoCorrect={false}
+            style={styles.textInput}
+            onChangeText={(text) => this.setState({ answerInput: text })}
+            value={this.state.answerInput}
+          />
+          <TouchableOpacity style={styles.submitButton} onPress={this.onSubmitAnswer}>
+            <Text style={styles.buttonText}>{this.state.submitButtonText}</Text>
+          </TouchableOpacity>
+          <Text style={styles.answerResult}>{this.state.answerResult}</Text>
+        </View>
+      </View>
+    )
+  }
+
+  renderBody() {
+    if (this.state.finished) {
+      return (
+        <View style={styles.score}>
+          <Text style={styles.score}>
+            You got {this.state.score}/{this.state.currentTest.questions.length} correct!
+          </Text>
+          <TouchableOpacity style={styles.submitButton} onPress={this.onRetry}>
+            <Text style={styles.buttonText}>Retry</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    }
+    if (!_.isNil(this.state.currentTest)) {
+      return (
+        this.renderQuestion()
+      )
+    }
+    if (this.state.showTests) {
+      return (
+        this.renderTests()
+      )
+    }
+    return (
+      this.renderMenu()
+    )
   }
 
   render() {
-    const {navigate} = this.props.navigation
+    const { navigate } = this.props.navigation
     return (
-      <View style={styles.container}>
-          <TouchableOpacity style={styles.testButton} onPress={this.onTakeTest}>
-          <Text style={styles.buttonText}>Take Test</Text>
-          </TouchableOpacity>
-          
-          <TouchableOpacity style={styles.createButton} onPress={() => navigate('Links')}>
-          <Text style={styles.buttonText}>Create Test</Text>
-          </TouchableOpacity>
-      </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        {this.renderBody()}
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -60,9 +184,52 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
+  answerResult: {
+    color: "#48D654"
+  },
   buttonText: {
     fontSize: 28,
     fontWeight: "600",
     color: 'white',
-  }
+  },
+  listItem: {
+    backgroundColor: "#42C5F4",
+    alignItems: 'center',
+    padding: 20,
+  },
+  listText: {
+    fontSize: 28,
+    fontWeight: '400',
+  },
+  question: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  questionText: {
+    fontSize: 28,
+    fontWeight: '400',
+  },
+  score: {
+    margin: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scoreText: {
+    fontSize: 28,
+    fontWeight: '300',
+  },
+  submitButton: {
+    margin: 10,
+    height: 40,
+    width: 100,
+    backgroundColor: "#48D654",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
 });
